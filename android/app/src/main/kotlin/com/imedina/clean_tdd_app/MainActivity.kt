86571f79150c9db0d4fa712b5ff82d274511dde3
Isaias Medina <isamed92@gmail.com>
2023-05-16 22:09:58 -0300

    first commit: add clean folder structure

---
+++ android/app/src/main/kotlin/com/imedina/clean_tdd_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.imedina.clean_tdd_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:clean_tdd_app/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme().getTheme(),
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {}, child: const Icon(Icons.add)),
+      ),
+    );
+  }
+}
