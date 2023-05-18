3df536e89f757b21d48bb01530a16f583e5d3ea1
Isaias Medina <isamed92@gmail.com>
2023-05-17 23:14:17 -0300

    use case core implemented and domain layer complete

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,7 @@
+import 'package:clean_tdd_app/core/usecases/usecase.dart';
 import 'package:clean_tdd_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_tdd_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:clean_tdd_app/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_tdd_app/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:dartz/dartz.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -10,26 +11,25 @@ import 'get_concrete_number_trivia_test.mocks.dart';
 
 @GenerateMocks([NumberTriviaRepository])
 void main() {
-  late GetConcreteNumberTrivia usecase;
+  late GetRandomNumberTrivia usecase;
   late MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
-  const tNumber = 1;
   const tNumberTrivia = NumberTrivia(
     number: 1,
     text: 'test',
   );
 
-  test('should get trivia for the number from the repository', () async {
-    when(mockNumberTriviaRepository.getConcreteNumberTrivia(1))
+  test('should get trivia from the repository', () async {
+    when(mockNumberTriviaRepository.getRandomTrivia())
         .thenAnswer((_) async => const Right(tNumberTrivia));
-    final result = await usecase(const Params(number: tNumber));
+    final result = await usecase(const NoParams());
     expect(result, const Right(tNumberTrivia));
-    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verify(mockNumberTriviaRepository.getRandomTrivia());
     verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
 }
